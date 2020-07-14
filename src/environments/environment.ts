2341f704e958973071e1adcde60283fc7db40829
dinushchathurya <dinushchathurya21@gmail.com>
2020-07-14 09:02:47 +0530

    Initialize the project

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false ,
+  firebase = {
+    apiKey: "AIzaSyB4QUcBVECO6OpmnnjAau0QEf-kyszKKpk",
+    authDomain: "ionic-crud-e234f.firebaseapp.com",
+    databaseURL: "https://ionic-crud-e234f.firebaseio.com",
+    projectId: "ionic-crud-e234f",
+    storageBucket: "ionic-crud-e234f.appspot.com",
+    messagingSenderId: "918899147447",
+    appId: "1:918899147447:web:c55111ed15a674ae2bc332",
+    measurementId: "G-B0HZLZ2XMX"
+  }
+};
+
